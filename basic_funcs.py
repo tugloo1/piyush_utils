e6bf90b21c88a908cea73fb36a537452394fb4ac
Piyush Gupta <piyushg91@gmail.com>
2018-04-27 22:35:32 -0700

    Added method to get api key from file

--- basic_funcs.py
+++ basic_funcs.py
@@ -1,8 +1,15 @@
 
 
-
-BasicFuncs(object):
+class BasicFuncs(object):
     @staticmethod
     def write_to_file(file_name: str, file_content: str, encoding='utf-8'):
         with open(file_name, 'w', encoding=encoding) as f:
             f.write(file_content)
+
+    @staticmethod
+    def get_api_key_from_file(file_name: str, encoding='utf-8') -> str:
+        with open(file_name, 'r', encoding=encoding) as f:
+            file_content = f.read()
+        first_line = file_content.splitlines()[0]
+        api_key = first_line.lstrip().rstrip()
+        return api_key
