0ac85515e4f8355da34cea5ef1bbefad06056bfa
Piyush Gupta <piyushg91@gmail.com>
2018-04-27 22:59:02 -0700

    Added method to mock out file opening

--- base_test_case.py
+++ base_test_case.py
@@ -1,5 +1,5 @@
 from unittest import TestCase
-from mock import patch
+from mock import patch, mock_open
 
 
 class BaseTestCase(TestCase):
@@ -7,3 +7,12 @@ class BaseTestCase(TestCase):
     def setUpClass(cls):
         cls.requests_get_patch = patch('requests.get')
         cls.requests_post_patch = patch('requests.post')
+
+    @staticmethod
+    def get_file_open_patch(read_data: str):
+        """
+        :param read_data: Data that will be mocked as being part of the text file
+        :return:
+        """
+        m = mock_open(read_data=read_data)
+        return patch('builtins.open', m, create=True)
