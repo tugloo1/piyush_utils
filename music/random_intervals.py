239a4348c7a0cd1072e98a79453cae891ff8accc
Piyush Gupta <piyushg91@gmail.com>
2019-04-25 20:45:17 -0700

    Adding abjad

---
+++ music/random_intervals.py
@@ -0,0 +1,39 @@
+import abjad
+import random
+
+
+class RandomIntervals(object):
+    scale_shift = {
+        'C': 0,
+        'D': 2,
+        'E': 4,
+        'F': 5,
+        'G': 7,
+        'A': 9,
+        'B': 11
+    }
+    base_scale = [0, 2, 4, 5, 7, 9, 11, 12]
+
+
+    @staticmethod
+    def select_random_scale():
+        return random.sample(RandomIntervals.scale_shift.keys(), 1)[0]
+
+    @classmethod
+    def generate_n_random_notes(cls, n: int):
+        duration = abjad.Duration(1, 4)
+        scale = cls.select_random_scale()
+        print('selected scale is ' + scale)
+        random_scale = cls.scale_shift[scale]
+        notes = []
+        while n > len(cls.base_scale):
+            n -= len(cls.base_scale)
+            notes += random.sample(cls.base_scale, len(cls.base_scale))
+        notes += random.sample(cls.base_scale, n)
+        new_notes = [abjad.Note(i + random_scale, duration) for i in notes]
+        staff = abjad.Staff(new_notes)
+        abjad.show(staff)
+
+
+RandomIntervals.generate_n_random_notes(30)
+
