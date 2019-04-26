24dd7c22a9dd4d91e43e8ef72c45b52fae6aa5da
Piyush Gupta <piyushg91@gmail.com>
2019-04-26 00:01:48 -0700

    Method for generating sequence of intervals

--- music/random_intervals.py
+++ music/random_intervals.py
@@ -22,6 +22,15 @@ class RandomIntervals(object):
         return random.sample(RandomIntervals.scale_shift.keys(), 1)[0]
 
     @classmethod
+    def get_random_notes_for_a_scale(cls, n: int):
+        notes = []
+        while n > len(cls.base_scale):
+            n -= len(cls.base_scale)
+            notes += random.sample(cls.base_scale, len(cls.base_scale))
+        notes += random.sample(cls.base_scale, n)
+        return notes
+
+    @classmethod
     def generate_n_random_notes(cls, n: int, scale: str=None):
         duration = abjad.Duration(1, 4)
         if not scale:
@@ -29,17 +38,31 @@ class RandomIntervals(object):
         print('selected scale is ' + scale)
         signature = KeySignature(scale.lower(), 'major')
         random_scale = cls.scale_shift[scale]
-        notes = []
-        while n > len(cls.base_scale):
-            n -= len(cls.base_scale)
-            notes += random.sample(cls.base_scale, len(cls.base_scale))
-        notes += random.sample(cls.base_scale, n)
+        notes = cls.get_random_notes_for_a_scale(n)
         new_notes = [abjad.Note(i + random_scale, duration) for i in notes]
         staff = abjad.Staff(new_notes)
         abjad.attach(signature, staff[0])
         abjad.show(staff)
 
+    @classmethod
+    def generate_random_interval_sequences(cls, n: int, scale: str=None):
+        duration = abjad.Duration(1, 4)
+        if not scale:
+            scale = cls.select_random_scale()
+        print('selected scale is ' + scale)
+        signature = KeySignature(scale.lower(), 'major')
+        random_scale = cls.scale_shift[scale]
+        notes = cls.get_random_notes_for_a_scale(n)
+        new_notes = []
+        for note in notes:
+            new_notes.append(abjad.Note(random_scale, duration))
+            new_notes.append(abjad.Note(note + random_scale, duration))
+        staff = abjad.Staff(new_notes)
+        abjad.attach(signature, staff[0])
+        abjad.show(staff)
+
+
 
 if __name__ == '__main__':
-    RandomIntervals.generate_n_random_notes(20, 'Bb')
+    RandomIntervals.generate_random_interval_sequences(20, 'Bb')
 
