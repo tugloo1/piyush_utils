6ee1472d90abc70633fac13f6376207dc198dff7
Piyush Gupta <piyushg91@gmail.com>
2019-04-27 18:25:55 -0700

    Generate full scale

--- music/random_intervals.py
+++ music/random_intervals.py
@@ -15,44 +15,75 @@ class RandomIntervals(object):
         'B': 11
     }
     base_scale = [0, 2, 4, 5, 7, 9, 11, 12]
+    increment_scale = [0, 2, 2, 1, 2, 2, 2, 1]
+    reverse_scale = [0, -1, -2, -2, -2, -1, -2, -2]
+    even_intervals = [2, 5, 9, 12]
+    odd_intervals = [4, 7, 11]
+    piano_range = (-39, 48)
 
+    @classmethod
+    def get_full_scale(cls, key_scale: str):
+        scale_shift = cls.scale_shift[key_scale]
+        bottom_portion = []
+        last_played = scale_shift
+        while last_played > cls.piano_range[0]:
+            index = len(bottom_portion) % len(cls.reverse_scale)
+            last_played += cls.reverse_scale[index]
+            bottom_portion.append(last_played)
+
+        top_portion = []
+        last_played = scale_shift
+        while last_played < cls.piano_range[-1]:
+            index = len(top_portion) % len(cls.increment_scale)
+            last_played += cls.increment_scale[index]
+            top_portion.append(last_played)
+
+        return sorted(list(set(bottom_portion + top_portion)))
 
     @staticmethod
     def select_random_scale():
         return random.sample(RandomIntervals.scale_shift.keys(), 1)[0]
 
     @classmethod
-    def get_random_notes_for_a_scale(cls, n: int):
+    def get_random_notes_for_a_scale(cls, n: int, scale_type):
         notes = []
-        while n > len(cls.base_scale):
-            n -= len(cls.base_scale)
-            notes += random.sample(cls.base_scale, len(cls.base_scale))
-        notes += random.sample(cls.base_scale, n)
+        if scale_type == 'base':
+            scale = cls.base_scale
+        elif scale_type == 'even':
+            scale = cls.even_intervals
+        elif scale_type == 'odd':
+            scale = cls.odd_intervals
+        else:
+            raise Exception('Unhandled scale type ' + scale_type)
+        while n > len(scale):
+            n -= len(scale)
+            notes += random.sample(scale, len(scale))
+        notes += random.sample(scale, n)
         return notes
 
     @classmethod
-    def generate_n_random_notes(cls, n: int, scale: str=None):
+    def generate_n_random_notes(cls, n: int, scale: str=None, scale_type: str='base'):
         duration = abjad.Duration(1, 4)
         if not scale:
             scale = cls.select_random_scale()
         print('selected scale is ' + scale)
         signature = KeySignature(scale.lower(), 'major')
         random_scale = cls.scale_shift[scale]
-        notes = cls.get_random_notes_for_a_scale(n)
+        notes = cls.get_random_notes_for_a_scale(n, scale_type)
         new_notes = [abjad.Note(i + random_scale, duration) for i in notes]
         staff = abjad.Staff(new_notes)
         abjad.attach(signature, staff[0])
         abjad.show(staff)
 
     @classmethod
-    def generate_random_interval_sequences(cls, n: int, scale: str=None):
+    def generate_random_interval_sequences(cls, n: int, scale: str=None, scale_type: str='base'):
         duration = abjad.Duration(1, 4)
         if not scale:
             scale = cls.select_random_scale()
         print('selected scale is ' + scale)
         signature = KeySignature(scale.lower(), 'major')
         random_scale = cls.scale_shift[scale]
-        notes = cls.get_random_notes_for_a_scale(n)
+        notes = cls.get_random_notes_for_a_scale(n, scale_type=scale_type)
         new_notes = []
         for note in notes:
             new_notes.append(abjad.Note(random_scale, duration))
@@ -62,7 +93,6 @@ class RandomIntervals(object):
         abjad.show(staff)
 
 
-
 if __name__ == '__main__':
-    RandomIntervals.generate_random_interval_sequences(20, 'Bb')
+    RandomIntervals.generate_n_random_notes(100, 'Bb', 'even')
 
--- tests/random_intervals_test.py
+++ tests/random_intervals_test.py
@@ -9,3 +9,11 @@ class RandomIntervalsTest(TestCase):
         valid = [-1, 1, 3, 4, 6, 8, 10, 11, 13]
         o = RandomIntervals.get_possible_notes_with_range(-1, 14)
         self.assertEqual(o, valid)
+
+    def test_get_full_scale(self):
+        i = RandomIntervals.get_full_scale('C')
+        bottom = [-39, -37, -36, -34, -32, -31, -29, -27, -25, -24, -22, -20, -19, -17, -15, -13, -12,
+                 -10, -8, -7, -5, -3, -1, 0]
+        top = [2, 4, 5, 7, 9, 11, 12, 14, 16, 17, 19, 21, 23, 24, 26, 28, 29, 31, 33, 35, 36, 38, 40, 41, 43, 45, 47,
+               48]
+        self.assertEqual(bottom + top, i)
