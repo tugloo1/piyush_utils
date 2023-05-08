ad892573633baf752815ff02b6190d3854a86ae7
Piyush Gupta <piyushg91@gmail.com>
2023-05-08 15:26:31 -0700

    Commiting my progress. too lazy to type out what it is

--- tennis_time_finder.py
+++ tennis_time_finder.py
@@ -8,12 +8,16 @@ class Locations:
     cheviot_hills = "Cheviot Hills Pay Tennis"
     vermont_canyon = "Vermont Canyon Pay Tennis"
     westwood = "Westwood Pay Tennis"
+    stoner = "Stoner RC"
+
+today = arrow.now()
+one_week = arrow.now().shift(days=8)
 
 
 def trifit_tennis():
-    today = arrow.now().format("MM/DD/YYYY")
-    one_week = arrow.now().shift(days=8).format("MM/DD/YYYY")
-    response = requests.get('https://www.myiclubonline.com/iclub/scheduling/classSchedule?club=30075&lowDate={0}&highDate={1}&_=1634537506545'.format(today, one_week))
+    today_format = today.format("MM/DD/YYYY")
+    one_week_format = one_week.format("MM/DD/YYYY")
+    response = requests.get('https://www.myiclubonline.com/iclub/scheduling/classSchedule?club=30075&lowDate={0}&highDate={1}&_=1634537506545'.format(today_format, one_week_format))
     json_output = json.loads(response.text)
     for event in json_output:
         if event['eventName'] == 'Tennis' and event['enrolled'] == '0':
@@ -32,8 +36,11 @@ def la_tennis_parks_court_finder(selected_date: str, begin_time: str, location: 
               'frheadcount': '0',
               'display': 'Detail',
               'module': 'FR'}
-    exclusion_criteria = {'<h2>Unavailable</h2>This Facility Reservation time block is unavailable. Please select another time block.',
-                          '<h2>Unavailable</h2>The begin date and time have already passed.'}
+    exclusion_criteria = {
+        '<h2>Unavailable</h2>This Facility Reservation time block is unavailable. Please select another time block.',
+        '<h2>Unavailable</h2>The begin date and time have already passed.'
+    }
+
     response = requests.get("https://reg.laparks.org/web/wbwsc/webtrac.wsc/search.html", params=params)
     soup_parser = BeautifulSoup(response.content, features="html.parser")
     court_htmls = soup_parser.find_all("table", {"id": "frwebsearch_output_table"})
@@ -52,9 +59,9 @@ def la_tennis_parks_court_finder(selected_date: str, begin_time: str, location: 
     return availability
 
 
-def print_out_court_availability(selected_date: str, location: str):
-    print("Court Availability for " + location)
-    availability = la_tennis_parks_court_finder(selected_date, begin_time="8:00am", location=location)
+def print_out_court_availability(selected_date: str, location: str, begin_time: str = "8:00 am"):
+    print("Court Availability for {0} on {1}".format(location, selected_date))
+    availability = la_tennis_parks_court_finder(selected_date, begin_time=begin_time, location=location)
     # pprint.pprint(availability)
     court_availability_dict = {}
     for court, times in availability.items():
@@ -66,8 +73,24 @@ def print_out_court_availability(selected_date: str, location: str):
     for start_time in sorted(court_availability_dict.keys()):
         print("{0} -> {1}".format(start_time.format("h:mm a"), str(court_availability_dict[start_time])))
 
+def print_out_availability_for_next_week(location: str):
+    current_date = today
+    while current_date < one_week:
+        print_out_court_availability(current_date.format("MM/DD/YYYY"), location)
+        print("\n")
+        current_date = current_date.shift(days=1)
+    pass
+
+
+def check_all_courts_for_date_and_time(selected_date: str, begin_time: str):
+    for location in {Locations.cheviot_hills, Locations.stoner, Locations.westwood}:
+        print_out_court_availability(selected_date, location=location, begin_time=begin_time)
+
+
 
 if __name__ == "__main__":
     # trifit_tennis()
-    print_out_court_availability("4/20/2023", location=Locations.cheviot_hills)
+    # print_out_availability_for_next_week(Locations.cheviot_hills)
+    check_all_courts_for_date_and_time("05/12/2023", "5:00 pm")
+    # print_out_court_availability("05/12/2023", location=Locations.westwood)
 
